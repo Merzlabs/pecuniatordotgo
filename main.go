81e750dcee03c2af8375c611894b3a40951e2437
Niklas Merz <niklasmerz@linux.com>
2019-10-21 22:08:44 +0200

    Start Go test with wellknown API

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+// Based on https://gist.github.com/michaljemala/d6f4e01c4834bf47a9c4
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	certFile = flag.String("cert", "someCertFile", "A PEM eoncoded certificate file.")
+	keyFile  = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
+	caFile   = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate file.")
+)
+
+func main() {
+	_ = godotenv.Load("secrets/sandbox.env")
+	flag.Parse()
+
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(*caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool, _ := x509.SystemCertPool()
+	if caCertPool == nil {
+		caCertPool = x509.NewCertPool()
+	}
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+
+	// Do GET something
+	resp, err := client.Get(os.Getenv("PT_WELLKNOWN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Dump response
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(data))
+}
