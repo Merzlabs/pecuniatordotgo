e90992c56d38a104e31ecc9920527ce961ac4cd7
Niklas Merz <niklasmerz@linux.com>
2019-10-22 22:07:12 +0200

    Request consent

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -13,45 +14,97 @@ import (
 	"os"
 
 	"github.com/Merzlabs/pecuniatordotgo/xs2a"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
 var (
-	certFile  = flag.String("cert", "someCertFile", "A PEM eoncoded certificate file.")
-	keyFile   = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
-	caFile    = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate file.")
-	transport *http.Transport
+	certFile = flag.String("cert", "someCertFile", "A PEM eoncoded certificate file.")
+	keyFile  = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
+	caFile   = flag.String("CA", "someCertCAFile", "A PEM encoded CA's certificate file.")
+	client   *http.Client
 )
 
 func main() {
 	_ = godotenv.Load("secrets/sandbox.env")
 	flag.Parse()
-	setupTransport()
+	setupClient()
 
 	// AIS Consent
-	//TODO
+	consent := new(xs2a.ConsentResponse)
+	err := startConsent(consent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(consent.Status))
 
 	// Get endpoints
 	endpoints := new(xs2a.Endpoints)
-	err := getEndpoints(endpoints)
+	err = getEndpoints(endpoints)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println(string(endpoints.Authorization))
 }
 
-func getEndpoints(target interface{}) error {
-	resp, err := EncryptedGet(os.Getenv("PT_WELLKNOWN"))
+func startConsent(consent *xs2a.ConsentResponse) error {
+	accs := []xs2a.Account{xs2a.Account{IBAN: os.Getenv("PT_IBAN")}}
+	access := &xs2a.ConsentAccess{
+		Balances:     accs,
+		Transactions: accs,
+	}
+	creq := &xs2a.ConsentRequest{
+		Access:                   *access,
+		RecurringIndicator:       true,
+		ValidUntil:               "2019-10-30", //TODO generate usefull value
+		FrequencyPerDay:          4,
+		CombinedServiceIndicator: false,
+	}
+	data, err := json.Marshal(creq)
+	if err != nil {
+		return err
+	}
+	reader := bytes.NewReader(data)
+
+	contentType := "application/json"
+	headers := make(map[string]string)
+	headers["Content-Type"] = contentType
+	headers["X-Request-ID"] = uuid.New().String()
+	headers["TPP-Redirect-URI"] = os.Getenv("PT_TPPREDIRECTURI")
+	headers["TPP-Redirect-Preferred"] = "true"
+	url := BuildURL("/consents")
+	log.Print(string(data))
+
+	res, err := EncryptedPost(url, contentType, reader, headers)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
+	//----- debug
+	data, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(data))
+	//-------
 
-	return json.NewDecoder(resp.Body).Decode(target)
+	//return json.NewDecoder(res.Body).Decode(consent)
+	return nil
+}
+
+func getEndpoints(target interface{}) error {
+	res, err := EncryptedGet(os.Getenv("PT_WELLKNOWN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	return json.NewDecoder(res.Body).Decode(target)
 }
 
 // Based on https://gist.github.com/michaljemala/d6f4e01c4834bf47a9c4
-func setupTransport() {
+func setupClient() {
 	// Load client cert
 	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
 	if err != nil {
@@ -75,20 +128,18 @@ func setupTransport() {
 		RootCAs:      caCertPool,
 	}
 	tlsConfig.BuildNameToCertificate()
-	transport = &http.Transport{TLSClientConfig: tlsConfig}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client = &http.Client{Transport: transport}
 }
 
 // EncryptedGet uses the certificates for connecting to the API
 func EncryptedGet(url string) (*http.Response, error) {
-	client := &http.Client{Transport: transport}
-
 	// Do GET something
 	return client.Get(url)
 }
 
 // EncryptedPost uses the certificates for connecting to the API
 func EncryptedPost(url string, contenttype string, body io.Reader, header map[string]string) (*http.Response, error) {
-	client := &http.Client{Transport: transport}
 	req, err := http.NewRequest("POST", url, body)
 
 	if err != nil {
