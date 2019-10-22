a395d14ecbb670e06baf4fa238d825297a9fe64c
Niklas Merz <niklasmerz@linux.com>
2019-10-22 21:01:43 +0200

    Introduce Endpoints object

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Merzlabs/pecuniatordotgo
+
+go 1.12
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/json"
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
+	"github.com/Merzlabs/pecuniatordotgo/xs2a"
 	"github.com/joho/godotenv"
 )
 
@@ -24,18 +28,26 @@ func main() {
 	flag.Parse()
 	setupTransport()
 
+	// AIS Consent
+	//TODO
+
+	// Get endpoints
+	endpoints := new(xs2a.Endpoints)
+	err := getEndpoints(endpoints)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(endpoints.Authorization))
+}
+
+func getEndpoints(target interface{}) error {
 	resp, err := EncryptedGet(os.Getenv("PT_WELLKNOWN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	// Dump response
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(string(data))
+	return json.NewDecoder(resp.Body).Decode(target)
 }
 
 // Based on https://gist.github.com/michaljemala/d6f4e01c4834bf47a9c4
@@ -67,9 +79,36 @@ func setupTransport() {
 }
 
 // EncryptedGet uses the certificates for connecting to the API
-func EncryptedGet(url string) (res *http.Response, err error) {
+func EncryptedGet(url string) (*http.Response, error) {
 	client := &http.Client{Transport: transport}
 
 	// Do GET something
 	return client.Get(url)
+}
+
+// EncryptedPost uses the certificates for connecting to the API
+func EncryptedPost(url string, contenttype string, body io.Reader, header map[string]string) (*http.Response, error) {
+	client := &http.Client{Transport: transport}
+	req, err := http.NewRequest("POST", url, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+
+	// Do GET something
+	return client.Do(req)
+}
+
+// BuildURL creates the complete url form params and environment
+func BuildURL(suffix string) string {
+	url := url.URL{
+		Scheme: "https",
+		Host:   os.Getenv("PT_HOST") + ":" + os.Getenv("PT_PORT"),
+		Path:   os.Getenv("PT_PATH") + "/" + os.Getenv("PT_VERS") + suffix,
+	}
+	return url.String()
 }
